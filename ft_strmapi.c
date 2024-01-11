a79c20de9301451d9a00267ae8aca54e58bdb119
OSMSBR <156334598+OSMSBR@users.noreply.github.com>
2024-01-11 16:15:56 +0100

    Create ft_strmapi.c
    applying a function to each character of the string and returning a modified string in a new allocated memory .

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osebbar <osebbar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/27 15:31:10 by osebbar           #+#    #+#             */
+/*   Updated: 2023/12/31 23:18:06 by osebbar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	str = (char *)malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
